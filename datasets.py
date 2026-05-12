f733ee6b173d29ebdf116040fa200bf33244a069
Uehara Takuma <e245743@ie.u-ryukyu.ac.jp>
2026-05-12 10:07:57 +0900

    fisrt commit

---
+++ datasets.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+def load_linear_example1():
+    """
+    >>> X, Y = load_linear_example1()
+    >>> X[0]
+    array([1, 4])
+    >>> # int() でラップして、純粋な数値として評価させる
+    >>> int(Y[0])
+    7
+    """
+    X = np.array([[1, 4], [1, 8], [1, 13], [1, 17]])
+    Y = np.array([7, 10, 11, 14])
+    return X, Y
+
+def load_nonlinear_example1():
+  """
+    >>> import datasets2
+    >>> X, Y = datasets2.load_nonlinear_example1()
+    >>> ex_X = datasets2.polynominal2_features(X)
+    >>> ex_X
+    array([[ 1.  ,  0.  ,  0.  ],
+           [ 1.  ,  2.  ,  4.  ],
+           [ 1.  ,  3.9 , 15.21],
+           [ 1.  ,  4.  , 16.  ]])
+    >>> Y
+    array([4., 0., 3., 2.])
+  """
+  X = np.array([[1,0.0],[1,2.0], [1,3.9], [1,4.0]])
+  Y = np.array([4.0, 0.0, 3.0, 2.0])
+  return X, Y
+
+def polynomial2_features(input):
+    poly2 = input[:,1:] ** 2
+    return np.c_[input,poly2]
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    
